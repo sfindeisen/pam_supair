888d550e08589a4c6f706827b6b8bbe6b236a817
Stanislaw Findeisen <stanislaw@ik.me>
2026-08-24 14:48:06 +0200

    expand licensing information in the header

--- test_supair.c
+++ test_supair.c
@@ -1,12 +1,24 @@
 /**
  * Unit tests for the pure pairing logic in pam_supair.c.
  *
+ * Copyright (C) 2026 Stanislaw Findeisen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
  * The module source is included directly with PAM_SUPAIR_UNIT_TEST defined,
  * which excludes the PAM-dependent parts so the tests build and run without
  * libpam. Run via `make test`.
- *
- * Copyright (C) 2011 Stanislaw T. Findeisen <stf at eisenbits.com>
- * Licensed under the GNU General Public License v3 or later.
  */
 
 #define PAM_SUPAIR_UNIT_TEST
